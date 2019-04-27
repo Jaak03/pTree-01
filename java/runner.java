535061994c568e50c82f8e15fc77ee2e76e74c3f
Jaak <jacques.barnard03@gmail.com>
2019-04-27 07:48:04 +0200

    Created basic java program that I could use to hook the class on.
    In this program so far I am cycling through the arguments. Next I would like to use one of these arguments and send them to the class to return an encrypted password.

---
+++ java/runner.java
@@ -0,0 +1,11 @@
+public class runner
+{
+    public static void main(String[] args)
+    {
+        //display the arguments
+        for (String arg : args) 
+        {
+            System.out.println(arg);
+        }
+    }
+}
